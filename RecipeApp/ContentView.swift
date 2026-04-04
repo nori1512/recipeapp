d88e432091cdffe3f27687bc2afd9a8bd2f1b69b
nori <nori1715tsuki@gmail.com>
2026-04-04 16:46:28 +0900

    Initial Commit

---
+++ RecipeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by 大月教史 on 2026/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeAppTests/RecipeAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeAppTests.swift
+//  RecipeAppTests
+//
+//  Created by 大月教史 on 2026/04/04.
+//
+
+import Testing
+@testable import RecipeApp
+
+struct RecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
